03abe8c3bba60283e141a9098dadab20a0f5746a
sharanbl <sharanlokeshwar@gmail.com>
2020-10-05 20:42:38 -0500

    Some Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 
 //const App = () => {}
 function App() {
   return (
-    <>
-     <h1>Welcome to Shopping Mart</h1>
-    </>
+    <Router>
+    <Header />
+    <main className="py-3">
+      <Container>
+     <Route path="/" exact component={HomeScreen} />
+     <Route path="/product/:id" component={ProductScreen} />
+      </Container>
+    </main>
+    <Footer />
+    </Router>
   );
 }
 
